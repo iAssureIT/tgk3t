12c043e09918826a37d94a9a91e4df7f964f5a44
anaghakomawar <madhavir1982@gmail.com>
2019-06-21 22:01:12 +0530

    Anagha seprated the api and react code

--- api/admin/api/models/roles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const roleSchema = mongoose.Schema({
-	_id			: mongoose.Schema.Types.ObjectId,
-    role        : String,
-    createdAt   : Date,
-});
-
-module.exports = mongoose.model('roles',roleSchema);
--- api/admin/server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const app = require('./app'); // app file include
-const port = process.env.PORT || 3006;
-
-const server = http.createServer(app);
-
-server.listen(port);
