37104cca4725aafdfbd5153617999e6cec198d8b
Rushikesh Salunkhe <rushikeshsalunkhe101@gmail.com>
2019-11-28 21:31:14 +0530

    Rushikesh changes in search

--- webapp/tgkWebsite/src/blocks/SearchProperty/SearchProperty.js
+++ webapp/tgkWebsite/src/blocks/SearchProperty/SearchProperty.js
@@ -214,6 +214,54 @@ class SearchProperty extends Component {
 	}
 
 	handleLocation(event){
+
+	/*
+		https://stackoverflow.com/questions/11246758/how-to-get-unique-values-in-an-array
+
+		Array.prototype.contains = function(v) {
+		  for (var i = 0; i < this.length; i++) {
+		    if (this[i] === v) return true;
+		  }
+		  return false;
+		};
+
+		Array.prototype.unique = function() {
+		  var arr = [];
+		  for (var i = 0; i < this.length; i++) {
+		    if (!arr.contains(this[i])) {
+		      arr.push(this[i]);
+		    }
+		  }
+		  return arr;
+		}
+
+		var duplicates = [1, 3, 4, 2, 1, 2, 3, 8];
+		var uniques = duplicates.unique(); // result = [1,3,4,2,8]
+
+		console.log(uniques);
+
+	*/
+
+
+		Array.prototype.contains = function(v) {
+		  for (var i = 0; i < this.length; i++) {
+		    if (this[i] === v) return true;
+		  }
+		  return false;
+		};
+
+		Array.prototype.unique = function() {
+		  var arr = [];
+		  for (var i = 0; i < this.length; i++) {
+		    if (!arr.contains(this[i])) {
+		      arr.push(this[i]);
+		    }
+		  }
+		  return arr;
+		}
+
+
+
 		var location = this.refs.location.value;
 
 		this.setState({
@@ -223,9 +271,11 @@ class SearchProperty extends Component {
 			{
 			axios({
 			      method: 'get',
-			      url: 'http://locationapi.iassureit.com/api/subareas/get/searchresults/' + this.state.location,
+			      url: 'http://locationapi.iassureit.com/api/societies/get/searchresults/' + this.state.location,
+			      // url: 'http://locationapi.iassureit.com/api/subareas/get/searchresults/' + this.state.location,
 			    })				
 				.then((searchResults) => {
+					console.log("searchResults",searchResults);
 					if(searchResults.data.length>0){
 						var cities = searchResults.data.map(a=>a.cityName);
 						cities = [...new Set(cities)];
@@ -236,24 +286,45 @@ class SearchProperty extends Component {
 						var subareaName = searchResults.data.map(a=>a.subareaName);
 						subareaName = [...new Set(subareaName)];
 
+						var societyName = searchResults.data.map(a=>a.societyName);
+						societyName = [...new Set(societyName)];
+						console.log("1  societyName  = ",societyName);
+
 						for(let i=0; i<cities.length; i++) {
 							for(let j=0; j<areas.length; j++) {
 								areas[j] = areas[j] + ', ' + cities[i];
 							}
-						}
+						}					
+						console.log("areas = ",areas);
 
-						for(let i=0; i<cities.length; i++) {
-							for(let j=0; j<subareaName.length; j++) {
-								subareaName[j] = subareaName[j] + ', ' + cities[i];
-							}
+						for(let i=0; i<searchResults.data.length; i++) {
+							subareaName[i] = searchResults.data[i].subareaName + ', ' + 
+											 searchResults.data[i].areaName + ', ' + 
+											 searchResults.data[i].cityName;
+						}	
+						var uniqueSubareaName = subareaName.unique();
+
+						console.log("uniqueSubareaName = ",uniqueSubareaName);
+
+
+						for(let i=0; i<searchResults.data.length; i++) {
+							societyName[i] = searchResults.data[i].societyName + ', ' + 
+											 searchResults.data[i].subareaName + ', ' + 
+											 searchResults.data[i].areaName + ', ' + 
+											 searchResults.data[i].cityName;
 						}
+						var uniqueSocietyName = societyName.unique();
+						console.log("uniqueSocietyName = ",uniqueSocietyName);
+
+
 
 						var citiesAreas = cities.concat(areas);
-						var citiesAreassubAreas = citiesAreas.concat(subareaName);
+						var citiesAreasSubAreas = citiesAreas.concat(uniqueSubareaName);
+						var citiesAreasSubAreasSocieties = citiesAreasSubAreas.concat(uniqueSocietyName);
 
 
 						this.setState({
-							locSearchResults : citiesAreassubAreas,
+							locSearchResults : citiesAreasSubAreasSocieties,
 						});					
 
 					}
